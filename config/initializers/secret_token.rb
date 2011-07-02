864a0564d794f8c87f02a7781d833a5432ff87a7
Padi <marcrendlignacio@gmail.com>
2011-07-02 16:18:13 +0800

    Run rails script to generate default rails directories

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biography::Application.config.secret_token = '6af3265ae451a84b4782a6eecb5ef749b4f7796c189c2b9bc7890feea832dc3e2aa9c5a53b8b08fa3c73217157255a1f88dc0e7db6d17eb30f116ec35fa2f901'
