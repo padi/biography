88c71fb93f1c01362744df030fad9e3a936052be
Padi <marcrendlignacio@gmail.com>
2011-07-04 00:11:48 +0800

    Fixed view test.

--- spec/views/pages/home.html.haml_spec.rb
+++ spec/views/pages/home.html.haml_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "pages/home.html.haml" do
-  it "should display the title" do
+  it "should display the Anne Frank's picture" do
     render
-    rendered.should have_selector('h1', :content => "Anne Frank")
+    rendered.should have_selector('img', :src => "images/AnneFrankChair.jpg", :alt => "AnneFrankChair.jpg")
   end
 end
